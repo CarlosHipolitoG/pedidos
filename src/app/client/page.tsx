d215fede1f7636b9993e451a9c0af32df17e9f7f
Carlos Eusebio Hipólito Garzón <carlos.hipolito@servinformacion.com>
2025-07-14 20:15:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/app/client/page.tsx
@@ -0,0 +1,8 @@
+export default function ClientPage() {
+  return (
+    <div>
+      <h1>Client Page</h1>
+      <p>This is where the client will see the menu and place an order.</p>
+    </div>
+  );
+}
---
+++ src/app/waiter/page.tsx
@@ -0,0 +1,8 @@
+export default function WaiterPage() {
+  return (
+    <div>
+      <h1>Waiter Page</h1>
+      <p>This is where the waiter will log in and manage orders.</p>
+    </div>
+  );
+}
