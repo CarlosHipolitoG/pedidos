b81aeefeee50418b280994e510921750fe4adb4c
Carlos Eusebio Hipólito Garzón <carlos.hipolito@servinformacion.com>
2025-07-19 18:02:35 +0000

    404: NOT_FOUND Code: DEPLOYMENT_NOT_FOUND ID: iad1::zlwqm-1752948072736-

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  swcMinify: false, // Add this line
   images: {
     unoptimized: true,
   },
