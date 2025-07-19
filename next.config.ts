bd9e02a1cd26787b1a6dbb2e4c520f7f5c143f71
Carlos Eusebio Hipólito Garzón <carlos.hipolito@servinformacion.com>
2025-07-19 18:19:02 +0000

    al cargar el proyecto en git y vercel 404: NOT_FOUND Code: DEPLOYMENT_NO

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  swcMinify: false, // Add this line
+  swcMinify: false,
   images: {
     unoptimized: true,
   },
