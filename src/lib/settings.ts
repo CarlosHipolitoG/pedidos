09ed6b6be06c111b59c0982ed5d87a669e23c1d9
Carlos Eusebio Hipólito Garzón <carlos.hipolito@servinformacion.com>
2025-07-30 22:19:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -30,7 +30,7 @@ export function useSettings() {
 
         return {
             barName: generalSettings.barName,
-            logoUrl: generalSettings.logo_url,
+            logoUrl: generalSettings.logoUrl,
             backgroundUrl: generalSettings.background_url,
             promotionalImages: state.promotional_images || [],
             taxRate: generalSettings.taxRate ?? 19,
@@ -55,7 +55,7 @@ export const updateSettings = async (formState: Settings): Promise<void> => {
     .update({
       barName: formState.barName, 
       taxRate: formState.taxRate,
-      logo_url: formState.logoUrl,
+      logoUrl: formState.logoUrl,
       background_url: formState.backgroundUrl
     })
     .eq('id', 1);
